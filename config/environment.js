407d1c613ba4fdbcf29475977b2e2ea4b49fcb20
Jordan VanderZwaag <jvanderzwaag@enova.com>
2015-07-08 21:44:35 -0500

    basic login route and views

--- config/environment.js
+++ config/environment.js
@@ -14,6 +14,7 @@ module.exports = function(environment) {
     },
 
     APP: {
+      authorization: 'simple-auth-authorizer:devise'
       // Here you can pass flags/options to your application instance
       // when it is created
     }
