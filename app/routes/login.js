19017b140931626de8846d575a27ffe4647946be
Jordan VanderZwaag <jvanderzwaag@enova.com>
2015-07-08 23:27:45 -0500

    get token-based login working

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    authenticate: function() {
-      var data = this.getProperties('identification', 'password');
-      return this.get('session').authenticate('simple-auth-authenticator:devise', data);
-    }
-  }
 });
